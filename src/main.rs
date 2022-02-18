2d8e729ad60de397748ed3a3a881ba598ce8aaca
James Cordor <grassojames5@gmail.com>
2022-02-18 13:19:29 +1100

    Yuh

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use num_cpus::get as get_cpus;
+use std::{
+    ops::Range,
+    u128::{MAX, MIN},
+};
+
+const RANGE: Range<u128> = MIN..MAX;
 
 fn main() {
     let cpus = get_cpus();
-
     let pool = threadpool::Builder::new();
 }
